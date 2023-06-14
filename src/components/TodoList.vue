4c2f2a6cf3db115c2c8ce45064eb08f75c2a91a3
ctccts22 <ctccts22@gmail.com>
2023-06-14 18:07:07 +0900

    First Vue Practice for my backend API.

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <ul>
+      <li>할일 1</li>
+      <li>할일 2</li>
+      <li>할일 3</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data
+  name: "TodoList"
+}
+</script>
+
+<style scoped>
+
+</style>
